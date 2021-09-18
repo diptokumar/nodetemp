fb93777c27c78b4152bae720a7159da8b538a670
diptotkumar <diptakumar2@gmail.com>
2021-09-18 17:50:06 +0600

    chages

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use(
 );
 
 // Serving static files
-app.use(express.static(`${__dirname}/public`));
+// app.use(express.static(`${__dirname}/public`));
 
 // Test middleware
 app.use((req, res, next) => {
